ae9b244b4cfad8c9992d5e3ef81ca2112469cc8b
csydive <95734853+csydive@users.noreply.github.com>
2021-12-27 11:17:37 +0900

    비대칭 키 암호화 추가

--- src/main/java/com/config/ResourceServerConfiguration.java
+++ src/main/java/com/config/ResourceServerConfiguration.java
@@ -1,8 +1,13 @@
 package com.config;
 
 
+import java.io.IOException;
+
+import org.apache.commons.io.IOUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -10,6 +15,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationManager;
 import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
 @Configuration
 @EnableResourceServer
@@ -38,6 +44,20 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
 	public void configure(HttpSecurity http) throws Exception {
 		http.httpBasic().disable().anonymous().and().authorizeRequests().antMatchers("/user/**").authenticated()
 				.antMatchers("/public/**").permitAll();
+	}
+	
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+	  JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+	  Resource resource = new ClassPathResource("oauth2_publickey.txt");
+	  String publicKey = null;
+	  try {
+	      publicKey = IOUtils.toString(resource.getInputStream());
+	  } catch (final IOException e) {
+	      throw new RuntimeException(e);
+	  }
+	  converter.setVerifierKey(publicKey);
+	  return converter;
 	}
 }
 
